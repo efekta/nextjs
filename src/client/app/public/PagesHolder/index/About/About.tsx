9f6ec899cceef971eb59af9389ca68cfe0e9deb2
lilu <23082010qQ>
2022-03-13 19:25:13 +0300

    upd

--- src/client/app/public/PagesHolder/index/About/About.tsx
+++ src/client/app/public/PagesHolder/index/About/About.tsx
@@ -1,6 +1,6 @@
 import { useIsMedium } from '@client/hooks/useMediaQuery';
 import { useTypedDispatch } from '@client/hooks/useTypedDispatch';
-import Image from "next/image";
+import { aboutData } from './About.data';
 import { Button, Icon, Container, Title, Text } from '@/ui';
 
 import s from './About.module.scss';
@@ -9,17 +9,31 @@ export const About = (): JSX.Element => {
     const isLarge = useIsMedium();
     const { modals: { showPublicModal } } = useTypedDispatch();
 
+    // @ts-ignore
     return (
         <section className={s.about}>
             <Container>
                 <Title variant="h1">Что такое виртуальная eSIM от Глонасс Мобайл?</Title>
-                {/*<h1 className={s.titleH1}>Что такое виртуальная eSIM от Глонасс Мобайл?</h1>*/}
 
                 <div className={s.aboutRow}>
+
+
+                    {aboutData.map((i) => (
+                        <div key={i.id} className={s.aboutCol}>
+                            <div className={s.wrapperPicture}>
+                                <Icon className={s.image} icon={i.icon} width={183} height={177}/>
+                            </div>
+                            <Title variant="h2">{i.title}</Title>
+                            <Text>{i.description}</Text>
+                            {/*<Button onClick={() => showPublicModal({i.modal})} className={s.btn}>{i.buttonText}</Button>*/}
+                        </div>
+                    ))}
+
+
+
                     <div className={s.aboutCol}>
                         <div className={s.wrapperPicture}>
                             <Icon className={s.image} icon='sim' width={183} height={177}/>
-                            {/*<Image src='/img/svg/sim.svg' alt='Инновация' layout='fill' width={183} height={177} className={s.image}/>*/}
                         </div>
                         <Title variant="h2">Инновация</Title>
                         <Text>Виртуальная eSIM это новая технология, которая не требует отдельного SIM-слота в смартфоне,
@@ -33,7 +47,6 @@ export const About = (): JSX.Element => {
                     <div className={s.aboutCol}>
                         <div className={s.wrapperPicture}>
                             <Icon className={s.image} icon='phone' width={214} height={148}/>
-                            {/*<Image src='/img/picture_2.png' alt='Удобство' layout='fill' width={214} height={148} className={s.image}/>*/}
                         </div>
                         <Title variant="h2">Удобство</Title>
                         <Text>В отличие от обычной SIM-карты, с eSIM не нужно переставлять SIM-карты или использовать
@@ -42,8 +55,6 @@ export const About = (): JSX.Element => {
                             С виртуальной eSIM партнеры и родственники смогут дозвонится также и по
                             стандартному российскому номеру.
                         </Text>
-
-                        {/*<Button variant='primary' onClick={() => showPublicModal('auth')} className={cn(s.btn, s.button, { [s.active]: isActive })}>Подключить</Button>*/}
                         <Button variant='primary' className={s.btn} onClick={() => showPublicModal('auth')}>Подключить</Button>
                     </div>
                 </div>
